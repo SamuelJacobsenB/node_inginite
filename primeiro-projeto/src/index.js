2166593583bd7c136c6f0db66fd7c48953f24cd8
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-06-13 06:56:16 -0300

    Update

--- primeiro-projeto/src/index.js
+++ primeiro-projeto/src/index.js
@@ -7,6 +7,33 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request,response,next){
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({error: 'Customer not found!'});
+    };
+
+    request.customer = customer;
+
+    return next();
+};
+
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation)=>{
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+};
+
 app.post('/account',(request, response)=>{
     const { cpf, name } = request.body;
 
@@ -28,16 +55,49 @@ app.post('/account',(request, response)=>{
     return response.status(201).send();
 });
 
-app.get('/statement',(request,response)=>{
-    const { cpf } = request.headers;
+// app.use(verifyIfExistsAccountCPF);
 
-    const customer = customers.find((customer) => customer.cpf === cpf);
+app.get('/statement', verifyIfExistsAccountCPF, (request,response)=>{
+    const { customer } = request;
+    return response.json(customer.statement);
+});
 
-    if(!customer){
-        return response.status(400).json({erro: 'Customer not found!'});
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response)=>{
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit',
     };
 
-    return response.json(customer.statement);
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF,(request, response)=>{
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount){
+        return response.status(400).json({error: 'Insufficient funds!'});
+    };
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit',
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
 });
 
 app.listen(2024);
